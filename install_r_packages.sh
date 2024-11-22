897dc4f2a9bc714b0f523216376625ddbe6d8933
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-11-21 21:49:59 -0500

    Delete install_r_packages.sh

--- install_r_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install BiocManager if not already installed
-R -e "if (!requireNamespace('BiocManager', quietly = TRUE)) install.packages('BiocManager')"
-
-# Use BiocManager to install the necessary Bioconductor packages
-R -e "BiocManager::install(c('Biostrings', 'Rfastp'))"
