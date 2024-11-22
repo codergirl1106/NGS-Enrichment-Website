82c8621baac6f845717d4bbe8967a2a8fe780fd7
codergirl1106 <68753302+codergirl1106@users.noreply.github.com>
2024-11-21 21:36:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+import plotly.express as px
+import pandas as pd
+import subprocess
+import streamlit as st
+
+def my_cache(f):
+    @st.cache_data(max_entries=5, ttl=600)
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        return f(*args, **kwargs)
+    return inner
+
+@st.cache_data
+def load_data(r1_fastq, r2_fastq, primes):
+    r1_fastq = r1_fastq.getvalue()
+    r2_fastq = r2_fastq.getvalue()
+    primes = primes.getvalue()
+    
+    with open("./r1_fastq.fastq.gz", 'wb') as w:
+        w.write(r1_fastq)
+
+    with open("./r2_fastq.fastq.gz", 'wb') as w:
+        w.write(r2_fastq)
+
+    with open("./primes.csv", 'wb') as w:
+        w.write(primes)
+
+    print("hi")
+    process = subprocess.Popen(["Rscript", "./server.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print(process)
+    result = process.communicate()
+    print(result)
+
+def main():
+    st.title("NGS Enrichment Website\n")
+    st.markdown("---")
+    st.markdown("***Website Objective***")
+    st.markdown('''NGS data analysis of yeast display repertoires is to analyse the diversity of each selection step,
+                and to identify high-affinity binders by quantifying the relative abundance of different protein variants within the population after each selection round.''')
+    st.markdown("---")
+    st.markdown("***Parameters***")
+    st.markdown("R1 and R2 fastq files in .fastq.gz form")
+    st.markdown("A .csv file of primers used, i.e:")
+
+    data = [
+        {"Primer": "NGS #1", "Fwd Seq": "TCGAAGGCGGAGGGTCGGCTAGC", "Rev Seq": "CTTCGACCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #2", "Fwd Seq": "ACCTGAGCGGAGGGTCGGCTAGC", "Rev Seq": "TCAGGTCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #3", "Fwd Seq": "GCAATCGCGGAGGGTCGGCTAGC", "Rev Seq": "GATTGCCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #4", "Fwd Seq": "GAGATTGCGGAGGGTCGGCTAGC", "Rev Seq": "TAATCTCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #5", "Fwd Seq": "ATCACGGCGGAGGGTCGGCTAGC", "Rev Seq": "CGATGTCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #6", "Fwd Seq": "TTAGGCGCGGAGGGTCGGCTAGC", "Rev Seq": "TGACCACCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #7", "Fwd Seq": "ACAGTGGCGGAGGGTCGGCTAGC", "Rev Seq": "GCCAATCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #8", "Fwd Seq": "CAGATCGCGGAGGGTCGGCTAGC", "Rev Seq": "ACTTGACCTCTTCAGAAATAAGCTTTTGTTCGGATCC"},
+        {"Primer": "NGS #9", "Fwd Seq": "ATGCACGCGGAGGGTCGGCTAGC", "Rev Seq": "TACATGCCTCTTCAGAAATAAGCTTTTGTTCGGATCC"}
+    ]
+
+    st.dataframe(pd.DataFrame(data))
+    st.markdown("---")
+    st.markdown("***Github*** [link](%s)" % "https://github.com/codergirl1106/NGS-Enrichment-Website/")
+    st.markdown("---")
+
+    
+    r1_fastq = st.file_uploader("upload R1 fastq file")
+    r2_fastq = st.file_uploader("upload R2 fastq file")
+    primes = st.file_uploader("upload a file containing the PCA Primers")
+
+    if r1_fastq != None and r2_fastq != None and primes != None:
+
+        load_data(r1_fastq, r2_fastq, primes)
+
+        dna_seq = pd.read_csv("./dna_sequences.csv", index_col=0)
+
+        amino_seq = pd.read_csv("./amino_sequences.csv", index_col=0)
+
+        dna_sequences_length = list(map(lambda x: len(x), dna_seq["sequence"].tolist()))
+
+        amino_acid_sequences_length = list(map(lambda x: len(x), amino_seq["sequence"].tolist()))
+
+        dna_occurrence_dict = {num: dna_sequences_length.count(num) for num in set(dna_sequences_length)}
+
+        amino_acid_occurrence_dict = {num: amino_acid_sequences_length.count(num) for num in set(amino_acid_sequences_length)}
+
+        dna_distribution = pd.DataFrame(list(dna_occurrence_dict.items()), columns=["Length of Merged DNA Sequences (bp)", "Counts"])
+
+        amino_acid_distribution = pd.DataFrame(list(amino_acid_occurrence_dict.items()), columns=["Length of Amino Acid Sequences", "Counts"])
+
+        dna_dist = px.histogram(dna_distribution, x="Length of Merged DNA Sequences (bp)", y="Counts", nbins=100)
+
+        dna_dist
+        
+        st.dataframe(dna_seq)
+
+        amino_dist = px.histogram(amino_acid_distribution, x="Length of Amino Acid Sequences", y="Counts", nbins=100)
+
+        amino_dist
+        
+        st.dataframe(amino_seq)
+
+main()
+
---
+++ server.R
@@ -0,0 +1,131 @@
+print("HIIIIIIII")
+
+options(repos = c(CRAN = "https://cloud.r-project.org/"))
+
+install.packages("BiocManager")
+
+BiocManager::install("Biostrings")
+BiocManager::install("Rfastp")
+
+library(Biostrings)
+library(Rfastp)
+library(ggplot2)
+library(data.table)
+library(stringr)
+library(tibble)
+library(dplyr)
+library(bslib)
+
+print("WHOOOOOO")
+
+merging <- function(file1, file2, out) {
+  o <- rfastp(read1 = file1, read2 = file2,
+              merge = TRUE, outputFastq = "", mergeOut = out, thread = 1)
+}
+
+convert_StringSet_to_df = function(stringset, combine_duplicates = T) {
+  sequence_df = as_tibble(as.character(stringset))
+  names(sequence_df) = "sequence"
+  if (combine_duplicates) {
+    sequence_df = sequence_df %>%
+      group_by(sequence) %>%
+      summarise(read_count = n()) %>%
+      ungroup() %>%
+      arrange(desc(read_count), sequence)
+  } else {
+    sequence_df = sequence_df %>%
+      mutate(read_count = 1)
+  }
+  return(as.data.frame(sequence_df))
+}
+
+translate_reads = function(reads) {
+  translations = translate(reads, if.fuzzy.codon = "solve")
+  return(as.character(translations))
+}
+
+temp_output <- "./temp_fastq_output.fastq.gz"
+
+merging("./r1_fastq.fastq.gz", "./r2_fastq.fastq.gz", temp_output)
+    
+fasta <- readLines(temp_output)
+      
+file.remove(temp_output)
+
+r1 <- readLines("./r1_fastq.fastq.gz")
+r2 <- readLines("./r2_fastq.fastq.gz")
+
+head <- r1[seq(1, length(r1), by = 4)]
+r1 <- r1[seq(2, length(r1), by = 4)]
+r2 <- as.vector(reverseComplement(DNAStringSet(r2[seq(2, length(r2), by = 4)])))
+
+hi <- data.table(id = fasta[seq(1, length(fasta), by = 4)], overlap = fasta[seq(2, length(fasta), by = 4)])
+hi$r1 <- r1[which(head %in% hi$id)]
+hi$r2 <- r2[which(head %in% hi$id)]
+
+hi[, combined := paste0(hi$r1, substr(hi$r2, nchar(hi$overlap) + 1, nchar(hi$r2)))][]
+
+sequences <- hi$combined
+
+primers <- fread("./primes.csv", header=TRUE)
+
+out <- NULL
+for (row in 1:nrow(primers)) {
+  F_seq <- as.character(primers[row,"Fwd Seq"])
+  R_seq <- as.character(primers[row,"Rev Seq"])
+  
+  n1 <- unlist(str_extract_all(sequences, paste0("^", F_seq, "(.*)", as.vector(reverseComplement(DNAStringSet(R_seq)))[1], "$")))
+  n2 <- DNAStringSet(as.vector(reverseComplement(DNAStringSet(unlist(str_extract_all(sequences, paste0("^", R_seq, "(.*)", as.vector(reverseComplement(DNAStringSet(F_seq)))[1], "$")))))))
+  
+  n1 <- substring(n1, nchar(F_seq) + 1, nchar(n1) - nchar(R_seq))
+  n2 <- substring(n2, nchar(R_seq) + 1, nchar(n2) - nchar(F_seq))
+  
+  n1 <- n1[n1 != ""] %>% convert_StringSet_to_df()
+  n2 <- n2[n2 != ""] %>% convert_StringSet_to_df()
+  
+  colnames(n1)[colnames(n1) == "read_count"] <- primers[row,]$Primer
+  colnames(n2)[colnames(n2) == "read_count"] <- primers[row,]$Primer
+  
+  comb <- rbind(n1, n2)
+  
+  comb <- comb %>% group_by(sequence) %>% summarise(across(primers[row,]$Primer, ~sum(.x, na.rm = TRUE)))
+  
+  if (is.null(out)) {
+    out <- comb
+  } else {
+    out <- merge(out, comb, all = TRUE, by = "sequence")
+    out[is.na(out)] <- 0
+  }
+}
+
+write.csv(out, file="dna_sequences.csv")
+
+dna0 <- out
+dna1 <- dna0
+dna1$sequence <- substring(dna1$sequence, 2)
+dna2 <- dna0
+dna2$sequence <- substring(dna2$sequence, 3)
+
+dna0$sequence <- translate_reads(DNAStringSet(dna0$sequence))
+dna0$id <- 1:nrow(dna0)
+dna0 <- dna0 %>% filter((str_ends(sequence, "TVSS") | str_ends(sequence, "TVSA")) & (str_starts(sequence, "Q") | str_starts(sequence, "E")))
+
+dna1$sequence <- translate_reads(DNAStringSet(dna1$sequence))
+dna1$id <- 1:nrow(dna1)
+dna1 <- dna1 %>% filter((str_ends(sequence, "TVSS") | str_ends(sequence, "TVSA")) & (str_starts(sequence, "Q") | str_starts(sequence, "E")))
+
+dna2$sequence <- translate_reads(DNAStringSet(dna2$sequence))
+dna2$id <- 1:nrow(dna2)
+dna2 <- dna2 %>% filter((str_ends(sequence, "TVSS") | str_ends(sequence, "TVSA")) & (str_starts(sequence, "Q") | str_starts(sequence, "E")))
+
+dna <- bind_rows(dna0, dna1, dna2)
+
+dna <- dna %>% distinct(id, .keep_all = TRUE)
+
+dna$id <- NULL
+
+dna <- dna %>% group_by(sequence) %>% summarise(across(where(~ !is.character(.) & !is.factor(.)), ~sum(.x, na.rm = TRUE)))
+
+dna <- dna[dna$sequence != "", ]
+
+write.csv(dna, file="./amino_sequences.csv")
